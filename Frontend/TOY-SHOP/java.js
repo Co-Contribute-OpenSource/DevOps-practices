777fa689202693116d916b3f4433fc21991975a3
architcoder5432 <archit.choudhary2001@gmail.com>
2023-10-18 12:12:31 +0530

    added a frontend project

---
+++ Frontend/TOY-SHOP/java.js
@@ -0,0 +1,13 @@
+const header = document.querySelector("header")
+
+window.addEventListener ("scroll", function(){
+    header.classList.toggle ("sticky", this.window.scrollY > 0);
+})
+
+let menu = document.querySelector('#menu-icon');
+let navmenu = document.querySelector('.navmenu');
+
+menu.oneclick = () => {
+    menu.classList.toggle('bx-x');
+    navmenu.classList.toggle('open');
+}
