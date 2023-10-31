885e3c4dd5d50dc0cc190c389ba6e9f0af631fd3
architcoder5432 <archit.choudhary2001@gmail.com>
2023-10-31 22:41:25 +0530

    added dark mode

--- Frontend/TOY-SHOP/java.js
+++ Frontend/TOY-SHOP/java.js
@@ -1,13 +1,23 @@
-const header = document.querySelector("header")
+const header = document.querySelector("header");
 
-window.addEventListener ("scroll", function(){
-    header.classList.toggle ("sticky", this.window.scrollY > 0);
-})
+window.addEventListener("scroll", function () {
+    header.classList.toggle("sticky", this.window.scrollY > 0);
+});
 
 let menu = document.querySelector('#menu-icon');
 let navmenu = document.querySelector('.navmenu');
 
-menu.oneclick = () => {
+menu.onclick = () => {
     menu.classList.toggle('bx-x');
     navmenu.classList.toggle('open');
-}
+};
+
+document.addEventListener('DOMContentLoaded', function () {
+    const darkModeToggle = document.getElementById('dark-mode-toggle');
+    const body = document.body;
+
+    darkModeToggle.addEventListener('click', function () {
+        body.classList.toggle('dark-mode');
+    });
+});
+
